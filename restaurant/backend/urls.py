caf90dad10bbdcaadbe04650db1978d268276673
BogdanaGonserovska <bogdanag51@gmail.com>
2023-01-11 15:02:04 +0200

    add, get, put menu

--- restaurant/backend/urls.py
+++ restaurant/backend/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('hello', views.hello_world, name='hello'),
     path('register', views.create_user, name='register'),
     path('logout', views.logout, name='logout'),
     path('users', views.get_users, name='users'),
     path('user', views.user_view, name='user'),
     path('consumers', views.get_consumers, name='consumers'),
     path('restaurants', views.get_restaurants, name='restaurants'),
+    path('menu', views.menu_view, name='menu'),
+    path('menu/<int:id>', views.get_menu, name='menu_id'),
 ]
